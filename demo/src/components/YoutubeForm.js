5db9ac53b9bb1fafd1fb2f84c671e8df8a6a003d
Juan Ocho <juaneme8@gmail.com>
2020-10-27 16:37:56 -0300

    Validation errors only showed when the form field has been visited

--- demo/src/components/YoutubeForm.js
+++ demo/src/components/YoutubeForm.js
@@ -34,23 +34,39 @@ function YoutubeForm() {
 		validate,
 	});
 
+	// console.log('Visited Fields', formik.touched);
+
 	return (
 		<div>
 			<form onSubmit={formik.handleSubmit}>
 				<div className='form-control'>
 					<label htmlFor='name'>Name</label>
-					<input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
-					{formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
+					<input type='text' id='name' name='name' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} />
+					{formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
 				</div>
 				<div className='form-control'>
 					<label htmlFor='email'>Email</label>
-					<input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
-					{formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
+					<input
+						type='email'
+						id='email'
+						name='email'
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						value={formik.values.email}
+					/>
+					{formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
 				</div>
 				<div className='form-control'>
 					<label htmlFor='channel'>Channel</label>
-					<input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} />
-					{formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
+					<input
+						type='text'
+						id='channel'
+						name='channel'
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						value={formik.values.channel}
+					/>
+					{formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
 				</div>
 				<button type='submit'>Submit</button>
 			</form>
