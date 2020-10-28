2e63cc1e0a2b2521f10f808efc5914f068298b16
Juan Ocho <juaneme8@gmail.com>
2020-10-28 16:24:40 -0300

    Starting with render props pattern

--- demo/src/components/YoutubeForm2.js
+++ demo/src/components/YoutubeForm2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 function YoutubeForm() {
@@ -7,6 +7,8 @@ function YoutubeForm() {
 		name: '',
 		email: '',
 		channel: '',
+		comments: '',
+		address: '',
 	};
 
 	const onSubmit = values => {
@@ -20,27 +22,38 @@ function YoutubeForm() {
 	});
 
 	return (
-		<Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-			<h1>¡Bienvenidos a YoutubeForm2!</h1>
-			<Form>
-				<div className='form-control'>
-					<label htmlFor='name'>Name</label>
-					<Field type='text' id='name' name='name' />
-					{formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
-				</div>
-				<div className='form-control'>
-					<label htmlFor='email'>Email</label>
-					<Field type='email' id='email' name='email' />
-					{formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
-				</div>
-				<div className='form-control'>
-					<label htmlFor='channel'>Channel</label>
-					<Field type='text' id='channel' name='channel' />
-					{formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
-				</div>
-				<button type='submit'>Submit</button>
-			</Form>
-		</Formik>
+		<>
+			<Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+				<Form>
+					<div className='form-control'>
+						<label htmlFor='name'>Name</label>
+						<Field type='text' id='name' name='name' placeholder='Nombre de usuario' />
+						<ErrorMessage name='name' />
+					</div>
+					<div className='form-control'>
+						<label htmlFor='email'>Email</label>
+						<Field type='email' id='email' name='email' />
+						<ErrorMessage name='email' />
+					</div>
+					<div className='form-control'>
+						<label htmlFor='channel'>Channel</label>
+						<Field type='text' id='channel' name='channel' />
+						<ErrorMessage name='channel' />
+					</div>
+					<div className='form-control'>
+						<label htmlFor='comments'>Comments</label>
+						<Field as='textarea' id='comments' name='comments' />
+						<ErrorMessage name='comments' />
+					</div>
+					<div className='form-control'>
+						<label htmlFor='address'>address</label>
+						<Field type='text' name='address' />
+					</div>
+
+					<button type='submit'>Submit</button>
+				</Form>
+			</Formik>
+		</>
 	);
 }
 
