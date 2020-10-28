cfcd65de92c0e9068ffa2e44a94cb9d97125e6e5
Juan Ocho <juaneme8@gmail.com>
2020-10-28 00:36:53 -0300

    Component Form & Field added but still not working

--- demo/src/components/YoutubeForm2.js
+++ demo/src/components/YoutubeForm2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
 function YoutubeForm() {
@@ -22,24 +22,24 @@ function YoutubeForm() {
 	return (
 		<Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
 			<h1>¡Bienvenidos a YoutubeForm2!</h1>
-			<form onSubmit={formik.handleSubmit}>
+			<Form>
 				<div className='form-control'>
 					<label htmlFor='name'>Name</label>
-					<input type='text' id='name' name='name' {...formik.getFieldProps('name')} />
+					<Field type='text' id='name' name='name' />
 					{formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
 				</div>
 				<div className='form-control'>
 					<label htmlFor='email'>Email</label>
-					<input type='email' id='email' name='email' {...formik.getFieldProps('email')} />
+					<Field type='email' id='email' name='email' />
 					{formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
 				</div>
 				<div className='form-control'>
 					<label htmlFor='channel'>Channel</label>
-					<input type='text' id='channel' name='channel' {...formik.getFieldProps('channel')} />
+					<Field type='text' id='channel' name='channel' />
 					{formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
 				</div>
 				<button type='submit'>Submit</button>
-			</form>
+			</Form>
 		</Formik>
 	);
 }
