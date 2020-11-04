181cea53721cbf89bf959e1fedaef408fe627a7f
Juan Ocho <juaneme8@gmail.com>
2020-11-04 20:50:01 -0300

    Input formik control

--- controls/src/components/FormikContainer.js
+++ controls/src/components/FormikContainer.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
+import FormikControl from './FormikControl';
 
 function FormikContainer() {
-	const initialValues = {};
-	const validationSchema = Yup.object({});
+	const initialValues = { email: '' };
+	const validationSchema = Yup.object({ email: Yup.string().email('Formato Inválido').required('Requerido') });
 	const onSubmit = values => console.log('Form Data', values);
 	return (
 		<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
 			{formik => (
 				<Form>
+					<FormikControl control='input' type='email' label='Email' name='email' />
+
 					<button type='submit'>Submit</button>
 				</Form>
 			)}
