95723e12152af1e3763a65a1c3cb08de36d81739
Juan Ocho <juaneme8@gmail.com>
2020-11-17 10:48:42 -0300

    Checkbox FormikControl

--- controls/src/components/FormikContainer.js
+++ controls/src/components/FormikContainer.js
@@ -15,12 +15,18 @@ function FormikContainer() {
 		{ key: 'Option 2', value: 'rOption2' },
 		{ key: 'Option 3', value: 'rOption3' },
 	];
-	const initialValues = { email: '', description: '', selectOption: '', radioOption: '' };
+	const checkboxOptions = [
+		{ key: 'Option 1', value: 'cOption1' },
+		{ key: 'Option 2', value: 'cOption2' },
+		{ key: 'Option 3', value: 'cOption3' },
+	];
+	const initialValues = { email: '', description: '', selectOption: '', radioOption: '', checkboxOption: [] };
 	const validationSchema = Yup.object({
 		email: Yup.string().email('Formato Inválido').required('Requerido'),
 		description: Yup.string().required('Requerido'),
 		selectOption: Yup.string().required('Requerido'),
 		radioOption: Yup.string().required('Requerido'),
+		checkboxOption: Yup.array().required('Requerido'),
 	});
 	const onSubmit = values => console.log('Form Data', values);
 	return (
@@ -31,6 +37,7 @@ function FormikContainer() {
 					<FormikControl control='textarea' label='Description' name='description' />
 					<FormikControl control='select' label='Select a Topic' name='selectOption' options={dropdownOptions} />
 					<FormikControl control='radio' label='Radio Topic' name='radioOption' options={radioOptions} />
+					<FormikControl control='checkbox' label='Checkbox Topics' name='checkboxOption' options={checkboxOptions} />
 
 					<button type='submit'>Submit</button>
 				</Form>
--- controls/src/components/FormikControl.js
+++ controls/src/components/FormikControl.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Input from './Input';
-import RadioButtons from './RadioButtons';
-import Select from './Select';
 import Textarea from './Textarea';
+import Select from './Select';
+import RadioButtons from './RadioButtons';
+import CheckboxGroup from './CheckboxGroup';
 
 function FormikControl(props) {
 	const { control, ...rest } = props;
@@ -16,6 +17,7 @@ function FormikControl(props) {
 		case 'radio':
 			return <RadioButtons {...rest} />;
 		case 'checkbox':
+			return <CheckboxGroup {...rest} />;
 		case 'date':
 		default:
 			return null;
